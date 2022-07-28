8d036dfccf48c96e12021e0834150e4cf74df488
Crany <ijeomahorjia@gmail.com>
2022-07-28 20:34:56 +0100

    Editted Inputs createID()

--- util/createID.js
+++ util/createID.js
@@ -1,6 +1,6 @@
 const randomWords = require('random-words');
 
-module.exports = (letterCount) => {
-    if (letterCount == null) letterCount = 5
-    return randomWords({exactly: 3, maxLength: letterCount, join: "."})
+module.exports = (max) => {
+    if (max == null) max = 5
+    return randomWords({exactly: 3, maxLength: max, join: "."})
 }
